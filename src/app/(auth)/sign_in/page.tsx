4de8444859c828b6e5f9c54234d8f194073ff540
zxlee <napoleon.lee0114@gmail.com>
2025-09-21 11:57:33 +0800

    feat: implememt signin with credentials

--- src/app/(auth)/sign_in/page.tsx
+++ src/app/(auth)/sign_in/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthForm from "@/components/forms/AuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth.action";
 import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
@@ -10,7 +11,7 @@ const SignIn = () => {
         formType="SIGN_IN"
         schema={SignInSchema}
         defaultValues={{ email: "", password: "" }}
-        onSubmit={(data) => Promise.resolve({ success: true, data })}
+        onSubmit={signInWithCredentials}
       />
     </>
   );
--- src/lib/actions/auth.action.ts
+++ src/lib/actions/auth.action.ts
@@ -11,7 +11,8 @@ import { ActionResponse, ErrorResponse } from "@/types/global";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { SignUpSchema } from "../validations";
+import { NotFoundError } from "../http-errors";
+import { SignInSchema, SignUpSchema } from "../validations";
 
 const PASSWORD_SALT_COUNT = 12;
 
@@ -61,7 +62,6 @@ export async function signUpWithCredentials(
 
     return { success: true };
   } catch (error) {
-    console.log({ authError: error });
     if (session.inTransaction()) {
       await session.abortTransaction();
     }
@@ -69,5 +69,39 @@ export async function signUpWithCredentials(
     return handleError(error) as ErrorResponse;
   } finally {
     await session.endSession();
+  }
+}
+
+export async function signInWithCredentials(
+  params: Pick<AuthCredentials, "email" | "password">
+): Promise<ActionResponse> {
+  const validationResult = await action({ params, schema: SignInSchema });
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { email, password } = validationResult.params!;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) throw new NotFoundError("User");
+
+    const existingAccount = await Account.findOne({
+      provider: "credentials",
+      providerAccountId: email,
+    });
+    if (!existingAccount) throw new NotFoundError("Account");
+
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      existingAccount.password
+    );
+    if (!isPasswordMatched) throw new Error("Password does not match");
+
+    await signIn("credentials", { email, password, redirect: false });
+
+    return { success: true };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
   }
 }
