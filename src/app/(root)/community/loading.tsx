406e2143d755facae8c735321839a61f77c4f82e
zxlee <napoleon.lee0114@gmail.com>
2025-10-05 16:51:19 +0800

    feat: (Community) add error & loading page for Community page

---
+++ src/app/(root)/community/loading.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Loading = () => {
+  return (
+    <section>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <Skeleton className="h-14 flex-1" />
+        <Skeleton className="h-14 w-28" />
+      </div>
+
+      <div className="mt-12 flex flex-wrap gap-5">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+          <Skeleton
+            key={item}
+            className="xs:w-[230px] h-60 w-full rounded-2xl"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
